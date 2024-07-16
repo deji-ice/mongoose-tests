8a976c3b5ebf623107624cc45c42703c12d06902
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-16 13:27:14 +0100

    feat: Add user registration functionality
    This commit adds the `createUser` function to the user controller, which handles the creation of a new user in the database. It also includes the corresponding route in the user route file. The purpose of this commit is to enable user registration in the application.

---
+++ controllers/userContoller.js
@@ -0,0 +1,14 @@
+import User from "../models/userModel.js"
+
+export const createUser = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json({
+            status: "success",
+            message: "User created successfully",
+        });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
