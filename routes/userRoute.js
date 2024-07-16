77eef669dd35764871824f6f637a012779c788db
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-16 17:43:37 +0100

    feat: Add user login functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,11 @@
 import express from "express"
-import { createUser } from "../controllers/userContoller.js"
+import { createUser, getAllUsers, userLogin } from "../controllers/userContoller.js"
+import { verifyJWToken } from "../middlewares/jwtAuth.js"
 
 const router = express.Router()
 
+router.get("/all",verifyJWToken, getAllUsers)
 router.post("/register", createUser)
+router.post("/login", userLogin)
 
 export default router
