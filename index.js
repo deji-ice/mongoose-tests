6179f5185b64066e8f36a3a509aa0535bf6b81b9
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-22 11:54:29 +0100

    chore: Add OTP generation functionality to user registration process

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import dbConnection from "./db/conn.js"
 import userRouter from "./routes/userRoute.js"
 import cors from "cors"
+import { OTPgenrator } from "./lib/OTPgenerator.js"
 dotenv.config()
 
 await dbConnection()
@@ -10,8 +11,9 @@ const app = express()
 const PORT = 3005
 app.use(express.json())
 app.use(cors())
-app.use("/api/v1/user", userRouter)
+app.use("/api/v1/user", userRouter)  
 
+OTPgenrator()  
 
 app.listen(PORT, () => {
 console.log(`server is running on port http://localhost:${PORT}`)
