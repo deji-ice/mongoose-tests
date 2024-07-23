b8280680abfb5766c5df3c4a7f2d18493c823769
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-23 13:30:48 +0100

    chore: Remove unused OTP generator import and function call

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv"
 import dbConnection from "./db/conn.js"
 import userRouter from "./routes/userRoute.js"
 import cors from "cors"
-import { OTPgenrator } from "./lib/OTPgenerator.js"
 dotenv.config()
 
 await dbConnection()
@@ -12,8 +11,6 @@ const PORT = 3005
 app.use(express.json())
 app.use(cors())
 app.use("/api/v1/user", userRouter)  
-
-OTPgenrator()  
 
 app.listen(PORT, () => {
 console.log(`server is running on port http://localhost:${PORT}`)
