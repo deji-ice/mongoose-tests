eeae0ca268559144e6d24f7973cdc1b86b7ea289
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-23 11:56:12 +0100

    feat: Add sendOTP and login_otp routes for OTP functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,26 @@
-import express from "express"
-import { createUser, getAllUsers, updatePassword, updateUserName, userLogin } from "../controllers/userContoller.js"
-import { verifyJWToken } from "../middlewares/jwtAuth.js"
+import express from "express";
+import {
+  createUser,
+  getAllUsers,
+  login_otp,
+  sendOTP,
+  updatePassword,
+  updatePasswordOTP,
+  updateUserName,
+  userLogin,
+} from "../controllers/userContoller.js";
+import { verifyJWToken } from "../middlewares/jwtAuth.js";
+import { validateOTP } from "../middlewares/validateOTP.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/all",verifyJWToken, getAllUsers)
-router.post("/register", createUser)
-router.post("/login", userLogin)
-router.patch("/update-password/:userName",verifyJWToken, updatePassword)
-router.patch("/update-userName/:userName", verifyJWToken,updateUserName)
+router.get("/all", verifyJWToken, getAllUsers);
+router.post("/register", createUser);
+router.post("/login", userLogin);
+router.patch("/update-password/:userName", verifyJWToken, updatePassword);
+router.patch("/update-userName/:userName", verifyJWToken, updateUserName);
+router.patch("/send-otp", sendOTP);
+router.post("/login-otp", validateOTP, login_otp);
+router.patch("/forgot-password/:email", verifyJWToken, updatePasswordOTP);
 
-export default router
+export default router;
