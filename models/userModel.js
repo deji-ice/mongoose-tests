4f515f141d1b7859bb4f6379c2f3db5914f1ca29
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-25 17:23:50 +0100

    feat: Update user model with lastName field and password hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,43 +1,51 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
+import crypto from "crypto";
 
 const userSchema = new Schema({
-    firstName:{
-        type: String,
-        required: true,
-        trim:true
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin", "staff"],
+    required: true,
+  },
+  secretKey: {
+    type: String,
+    default: function () {
+      return crypto.randomBytes(16).toString("hex");
     },
-    lastName:{
-        type: String,
-        required: true,
-        trim:true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim:true
-    },
-    userName:{
-        type: String,
-        required: true,
-        unique: true,
-        trim:true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    },
-    role: {
-    type:String,
-    default: "student",
-    }
-})
+  },
+});
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 export default User;
